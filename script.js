8abd5ce92cbe5d5771ed4c885d7a81fbc647dd6c
sheinken <heinken.sebastian@gmail.com>
2022-06-15 19:12:19 -0300

    final commit

--- script.js
+++ script.js
@@ -1,141 +1,129 @@
-const products = [
+let carts = document.querySelectorAll(".add-cart");
+
+let products = [
   {
-    id: 1,
     name: "New Balance Green",
-    price: 50,
-    image: "/img/img-1.jpg",
-  },
-  {
-    id: 2,
-    name: "Nike Brown",
+    tag: "New-Balance-Green",
     price: 60,
-    image: "/img/img-2.jpg",
+    inCart: 0,
   },
   {
-    id: 3,
-    name: "Nike Pink",
+    name: "Nike Casual Brown",
+    tag: "Nike-Casual-Brown",
+    price: 50,
+    inCart: 0,
+  },
+  {
+    name: "Nike Run Pink",
+    tag: "Nike-Run-Pink",
     price: 70,
-    image: "/img/img-3.jpg",
+    inCart: 0,
   },
   {
-    id: 4,
-    name: "Nike Red",
+    name: "Nike Air Red",
+    tag: "Nike-Air-Red",
     price: 80,
-    image: "/img/img-4.jpg",
+    inCart: 0,
   },
 ];
 
-// function to select elements from the DOM
-const select = (element) => document.querySelector(element);
-
-// function to create DOM elements
-const create = (element) => document.createElement(element);
-
-// select element by ID in DOM
-const productContainer = select("#products");
-
-// iterate throught array of products
-for (let product of products) {
-  // create div element in DOM
-  const div = create("div");
-
-  // extract product properties
-  const { id, name, price, image } = product;
-
-  div.innerHTML = `
-  <div class="product-container">
-        <h3>${name}</h3>
-        <img src="${image}" alt="" />
-        <h3>$${price}</h3>
-        <button class="button-add" id="addToCart-${id}">Add</button class = "button-add">
-      </div>`;
-
-  // add div to DOM
-  productContainer.append(div);
-
-  // select buttons by ID
-  const buttonAdd = select(`#addToCart-${id}`);
-
-  // add event listeners to buttons
-  // give parameter to callback
-  buttonAdd.addEventListener("click", () => addToCart(product));
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+  });
 }
 
-function addToCart(product) {
-  // get checkout from local storage
-  const checkout = getCheckout();
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
 
-  // find product in checkout by ID
-  const productInCheckout = checkout.find((item) => item.id === product.id);
+  if (productNumbers) {
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
+}
 
-  // if product is already in checkout, increase amount
-  if (productInCheckout) {
-    productInCheckout.quantity++;
-  } // if product is not in checkout, add
-  else {
-    product.quantity = 1;
-    checkout.push(product);
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".cart span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
   }
 
-  //save checkout in local storage
-  saveCheckout(checkout);
-
-  // show checkout in DOM
-  renderCheckout();
+  setItems(product);
 }
 
-// function to get checkout from local storage, if not found return empty array
-function getCheckout() {
-  return JSON.parse(localStorage.getItem("checkout")) || [];
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
-// function to save checkout in local storage
-function saveCheckout(checkout) {
-  localStorage.setItem("checkout", JSON.stringify(checkout));
+function totalCost(product) {
+  //   console.log("The product price is", product.price);
+  let cartCost = localStorage.getItem("totalCost");
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + product.price);
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
 }
 
-// function to render checkout
-function renderCheckout() {
-  const checkout = getCheckout();
-  const checkoutContainer = select("#checkout");
-  const quantity = select("#quantity");
-  const total = select("#total");
+function displayCart() {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  let productContainer = document.querySelector(".product-container");
+  let cartCost = localStorage.getItem("totalCost");
 
-  quantity.innerText = calculateQuantity(checkout);
-  total.innerText = "$" + calculateTotal(checkout);
+  if (cartItems && productContainer) {
+    // productContainer.innerHTML = "";
+    Object.values(cartItems).map((item) => {
+      productContainer.innerHTML += `
+      <div class="products">
+        <span>${item.name}</span>
+        <div class="price">$${item.price},00</div>
+        <div class="quantity">
+          <ion-icon name="caret-back-circle-outline"></ion-icon>
+          <span>${item.inCart}</span>
+          <ion-icon name="caret-forward-circle-outline"></ion-icon>
+        </div>
+        <div class="total">$${item.inCart * item.price},00</div>
+      </div>
+      
+      `;
+    });
 
-  checkoutContainer.innerHTML = "";
-
-  // iterate through product array in chekcout
-  for (let product of checkout) {
-    // extract product properties
-    const { name, price, quantity } = product;
-    const div = create("div");
-
-    // template to show prod in checkout
-    div.innerHTML = `
-    <div class="checkout">
-        <h5 id="quantity">Quantity: ${quantity}</h5>
-        <h5 id="total">Total: $${total}</h5>
-        <div id="checkout"></div>
-
+    productContainer.innerHTML += `
+      <div class="basket-total-container">
+        <h4 class="basket-total-title">Basket Total</h4>
+        <h4 class="basket-total">$${cartCost},00</h4>    
       </div>
     `;
-
-    // add div to DOM
-    checkoutContainer.append(div);
   }
 }
 
-// function to calculate total
-function calculateTotal(checkout) {
-  return checkout.reduce(
-    (total, item) => total + item.quantity * item.price,
-    0
-  );
-}
-
-// function to calculate quantity
-function calculateQuantity(checkout) {
-  return checkout.reduce((total, item) => total + item.quantity, 0);
-}
+onLoadCartNumbers();
+displayCart();
